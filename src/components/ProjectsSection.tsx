e5f16a7c56149a720094e72f4dda8a6457ad8f4d
Nalini123955 <ssnalini2004@gmail.com>
2025-09-04 07:12:27 -0700

    Update ProjectsSection.tsx

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -87,7 +87,7 @@ export const ProjectsSection = () => {
       tech: ["python","Django","Bootstrap","Mysql"],
       image: empsImg,
       github: "https://github.com/Nalini123955/employee-management-system.git",
-      demo: "https://emps4321.pythonanywhere.com/",
+      demo: "https://emps4321.pythonanywhere.com/emp/home/",
       category: "Web app",
     },
   ];
